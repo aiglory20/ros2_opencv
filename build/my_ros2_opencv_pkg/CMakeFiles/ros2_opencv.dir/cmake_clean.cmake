44d6b577041d88263f33908f7a5ca1f8e5c075a9
BinLing Feng <124499798+aiglory20@users.noreply.github.com>
2024-01-21 13:58:06 +0800

    Add files via upload

---
+++ build/my_ros2_opencv_pkg/CMakeFiles/ros2_opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros2_opencv.dir/src/ros2_opencv.cpp.o"
+  "ros2_opencv"
+  "ros2_opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros2_opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
